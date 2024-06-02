7c57ed96e327b64d45d38e12d3b2867928acbf6d
Meryem Marasli <mgmmeryem@gmail.com>
2024-06-02 18:41:26 -0400

    updating main page to have banner and map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,52 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="banner">
+      <div class="intro">
+        <h1 class="intro-line">Study Pitt!</h1>
+        <h2 class="intro-line">Find your next study spot.</h2>
+        <h3 class="intro-line">Let's get started</h3>
+      </div>
     </div>
+    <div class="map">
+      <div class="map-side">
+        <img alt="Map of University of Pittsburgh" src="https://www.ieee-iri.org/images/payments_subdomain/imgThack.gif" class="map-image"/>
+      </div>
+      <div class="map-list">
+        <h1 class="explore-title">Explore</h1>
+        <div class="scrollableContainer">
+          <ul>
+            <li>Cathedral of Learning</li>
+            <li>William Pitt Union</li>
+            <li>Sennott Square</li>
+            <li>Hillman Library</li>
+            <li>Information Science Buidling</li>
+            <li>Posvar</li>
+            <li>Thackery</li>
+            <li>Frick Fine Arts Building</li>
+            <li>Chevron Science Center</li>
+            <li>Mervis Hall</li>
+            <li>Lawerence Hall</li>
+            <li>Peterson Event Center</li>
+            <li>Benedum</li>
+            <li>Carnegie Library</li>
+            <li>Bellevue</li>
+            <li>Clapp</li>
+            <li>Langley</li>
+            <li>Crawford</li>
+            <li>Salk</li>
+            <li>Victoria</li>
+            <li>Scafe</li>
+            <li>O'Hara</li>
+            <li>Victoria</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+
+    </div>
+
   );
 }
 
